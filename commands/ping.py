734a2f24b2ce3d684b6cda13478fab6431c8a366
Kevin Korsbakke <kevin@korsbakke.se>
2015-08-30 21:18:27 +0200

    removed debug prints

--- commands/ping.py
+++ commands/ping.py
@@ -4,10 +4,8 @@ short_description = "Returns pong"
 def will_respond_to_msg(text):
 	words = text.split()
 	if words[0].lower() == "!ping":
-		print True #debug
 		return True
 	else:
-		print False #debug
 		return False
 
 def run_command(replyTo, text):
