841c9302c0d561c67e98a6f04330f33ae699fd34
SmexxarN <kevin@korsbakke.se>
2015-09-04 13:00:30 +0200

    Added json to get the password

--- commands/ts.py
+++ commands/ts.py
@@ -5,11 +5,10 @@ long_description = "Teamspeak Command - v1.0 \nUsage: !ts \nReturns a list of pe
 import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 import re
+import json
 
-"""
-Add json decode to figure out the password in a seperate file
-Otherwise the password will end up on github
-"""
+with open('config.json') as data_file:    
+    data = json.load(data_file)
 	
 def will_respond_to_msg(text):
 	words = text.split()
