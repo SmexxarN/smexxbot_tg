a068595d076dd9d9e5dc0407da120846ed235eee
SmexxarN <kevin@korsbakke.se>
2015-10-13 21:14:34 +0200

    Removed almost everything

--- commands/sudo.py
+++ commands/sudo.py
@@ -4,63 +4,7 @@ long_description = "Sudo Command - v1.1\n\nUsage: \n!sudo kick <username> \nKick
 
 import threading
 
-numberofvotes_yes = 0
-numberofvotes_no = 0
-chatinforesponse = ""
-replyTo2 = None
-listofstarts = {}
-userinfostring = ""
-user = ""
-sendlist = False
-vote_running = 0
 
-
-#Userinfo2
-phone = ""
-print_name = ""
-id = ""
-first_name = ""
-username = ""
-
-
-def userinfo(info, start):
-	return
-	
-
-def userinfo2():
-	return
-
-
-def perm_check(username):
-
-	admins = {"smexxarn", "dennip", "mowsh", "brunado"}
-	
-	for i in range(0, len(admins)):
-		if admins[i] == username:
-			return true
-
-			
-
-def vote_count(id, end):
-	global numberofvotes_yes
-	global numberofvotes_no
-	global vote_running
-	replyTo.send_msg("Current vote count:\nYes: " + numberofvotes_yes + "\nNo: " + numberofvotes_no)
-	if end == true:
-		if numberofvotes_yes >= 3:
-			replyTo.del_user("user#" + id)
-			replyTo.send_msg(id + " was kicked")
-		vote_running = 0
-
-
-
-def cb_function(extra, success, result):
-	if success:
-		tprint(result)
-		if sendlist:
-			send_msg(replyTo2, chatinforesponse, ok_cb, false)
-		else:
-			userinfo(chatinforesponse)
 
 def will_respond_to_msg(text):
 	words = text.split
