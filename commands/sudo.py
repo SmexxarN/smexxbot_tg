5386e25d8f39619742ca5764140df00d4552a89b
SmexxarN <kevin@korsbakke.se>
2015-10-13 21:15:19 +0200

    Sudo does nothing...

--- commands/sudo.py
+++ commands/sudo.py
@@ -2,8 +2,6 @@ command_name = "Sudo"
 short_description = "Sudo Command"
 long_description = "Sudo Command - v1.1\n\nUsage: \n!sudo kick <username> \nKicks the user from the group \n\n!sudo invite <username> \nInvites the user to the group \n\n!sudo list \nLists userinfo of everyone in the group "
 
-import threading
-
 
 
 def will_respond_to_msg(text):
